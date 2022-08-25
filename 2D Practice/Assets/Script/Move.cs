f97e5f9cb2e63507e070962ff69f9cd5be57055c
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-08-25 16:58:00 +0900

    2D테스트
    2D테스트

---
+++ 2D Practice/Assets/Script/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 Dir = Vector3.right * h + Vector3.up * v;
+
+        transform.position += Dir * 5.0f * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ㅎㅇ");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ㅂㅇ");
+
+    }
+}
